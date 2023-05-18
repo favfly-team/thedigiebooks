0a6a7ea1216bd6c4641919a635cd2e2dbeda69fb
Darpan Chakraborty <favfly.darpan@gmail.com>
2023-05-18 11:37:03 +0530

    update

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,10 +1,12 @@
 import { useEffect } from "react";
 import lozad from "lozad";
 import { Client } from "../utils/prismicHelpers";
+import PrismicClient from "../utils/prismicClient";
+import gql from "graphql-tag";
 import { SliceZone } from "../slices";
 import SEO from "../components/seo/SEO";
 
-const ServicePage = ({ doc }) => {
+const ServicePage = ({ doc, blogPosts }) => {
   // ========== LOZAD INSTANTIATE ==========
   useEffect(() => {
     const observer = lozad(".lozad", {
@@ -21,7 +23,10 @@ const ServicePage = ({ doc }) => {
         doc={doc}
         url={`https://${process.env.NEXT_PUBLIC_PRISMIC_ID}.${process.env.NEXT_PUBLIC_TLD}/${doc?.uid}`}
       />
-      <SliceZone sliceZone={doc.data.body} />
+      <SliceZone
+        sliceZone={doc.data.body}
+        blogsData={blogPosts?.data?.allBlog_posts?.edges}
+      />
     </>
   );
 };
@@ -49,9 +54,32 @@ export async function getServerSideProps({
     };
   }
 
+  const pClient = PrismicClient;
+
+  const blogPosts = await pClient.query({
+    query: gql`
+      query {
+        allBlog_posts(sortBy: published_date_DESC, first: 3) {
+          edges {
+            node {
+              title
+              description
+              featured_image
+              published_date
+              _meta {
+                uid
+              }
+            }
+          }
+        }
+      }
+    `,
+  });
+
   return {
     props: {
       doc,
+      blogPosts,
       preview,
     },
   };
